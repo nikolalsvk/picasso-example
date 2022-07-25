87b5f14eb8e9966729b3c6bf1b37ccf2590e45b6
Nikola Đuza <nikolaseap@gmail.com>
2022-07-25 10:10:47 +0200

    Rewire CRA to work with Picasso

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Picasso from '@toptal/picasso-provider'
+import { Button, Page, Container } from '@toptal/picasso'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Picasso>
+      <Page>
+        <Page.TopBar title='App Page' />
+        <Page.Content>
+          <Page.Article>
+            <Container top={7} bottom={7} flex justifyContent='center' direction='column'>
+              <Container flex bottom={2}>
+                Your application goes here
+              </Container>
+              <Container>
+                <Button>Hello world!</Button>
+              </Container>
+            </Container>
+          </Page.Article>
+        </Page.Content>
+        <Page.Footer />
+      </Page>
+    </Picasso>
   );
 }
 
